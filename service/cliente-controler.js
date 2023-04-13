e6733287c21f3bbe606086b25b5f2478f09b31e6
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-04-13 16:16:46 -0300

    inicio da edição de clientes

--- service/cliente-controler.js
+++ service/cliente-controler.js
@@ -1,41 +1,58 @@
-import { apiPost,apiDelete } from "./cliente-service.js";
+import { apiPost, apiDelete } from "./cliente-service.js";
 
-export const nome = document.querySelector('[data-nome]');
-export const email = document.querySelector('[data-email]');
-const container = document.querySelector('[data-tabela]')
-const form = document.querySelector("[data-form]")
+export const nome = document.querySelector("[data-nome]");
+export const email = document.querySelector("[data-email]");
+const container = document.querySelector("[data-tabela]");
+const form = document.querySelector("[data-form]");
 
-export const criaCliente = (cliente,id) => {
-    const novaLinha = document.createElement('tr');
-    const conteudo = `<td class="td" data-td>${cliente.nome}</td>
+export const criaCliente = (cliente, id) => {
+  const novaLinha = document.createElement("tr");
+  const conteudo = `<tr>
+    <td class="td" data-td>${cliente.nome}</td>
     <td>${cliente.email}</td>
     <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>`
+      <ul class="tabela__botoes-controle">
+        <li>
+          <button class="botao-simples botao-simples--editar">
+            <a href="./editar.html">Editar</a>
+          </button>
+        </li>
+        <li>
+          <button class="botao-simples botao-simples--excluir" type="button">Excluir</button>
+        </li>
+      </ul>
+    </td>
+  </tr>`;
 
-    novaLinha.innerHTML = conteudo
-    novaLinha.dataset.id = id
-    container.appendChild(novaLinha)
+  novaLinha.innerHTML = conteudo;
+  novaLinha.dataset.id = id;
+  container.appendChild(novaLinha);
+};
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    apiPost();
+    window.location.href = "../telas/cadastro_concluido.html";
+  });
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    apiPost()
-    window.location.href = '../telas/cadastro_concluido.html';
-})
+container.addEventListener("click", (e) => {
+  e.preventDefault();
+  const btnEditar = e.target.closest(".botao-simples--editar");
 
-form.addEventListener('click',(e) =>{
-    e.preventDefault()
+  if (btnEditar) {
+    const linha = btnEditar.closest("[data-id]");
+    const id = linha.dataset.id;
+    window.location.href = `../telas/editar.html?id=${id}`;
+  }
 
-    const btnExcluir = e.target.className = 'botao-simples botao-simples--excluir'
+  const btnExcluir =
+    e.target.className == "botao-simples botao-simples--excluir";
 
-    if(btnExcluir){
-        const linha = e.target.closest('[data-id]')
-        let id = linha.dataset.id
-        apiDelete(id)
-    }
-   
-})
+  if (btnExcluir) {
+    const linha = e.target.closest("[data-id]");
+    const id = linha.dataset.id;
+    apiDelete(id);
+  }
+});
--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,38 +1,34 @@
 import { criaCliente, nome, email } from "./cliente-controler.js";
-const url = 'http://localhost:3000/profile';
+const url = "http://localhost:3000/profile";
 
-const apiGet = async () =>{
-    const response = await fetch(url)
+const apiGet = async () => {
+  const response = await fetch(url);
 
-    const data = await response.json()
-    data.map((cliente) => criaCliente(cliente, cliente.id))
-}
+  const data = await response.json();
+  data.map((cliente) => criaCliente(cliente, cliente.id));
+};
 
-export const apiPost = async () =>{
-    const response = await fetch(url, {
-        method: "POST",
-        headers:{
-            "Content-type":"application/json"
-        },
-        body:JSON.stringify({
-            nome: nome.value,
-            email: email.value,
-        })
-    })
+export const apiPost = async () => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome.value,
+      email: email.value,
+    }),
+  });
 
-    const data = await response.json()
+  const data = await response.json();
 
-    criaCliente(data, data.id)
-}
+  criaCliente(data, data.id);
+};
 
+export const apiDelete = async (id) => {
+  const response = await fetch(`${url}/${id}`, {
+    method: "DELETE",
+  });
+};
 
-export const apiDelete = async (id) =>{
-    const response = await fetch(`${url}/${id}`, {
-        method: "DELETE",
-    })
-}
-
-apiGet()
-
-
-
+apiGet();
